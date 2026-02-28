506ce11c374c13c95e14023c0391e34cdf2c1a82
aiminilabs0 <aiminilabs0@gmail.com>
2026-02-27 23:34:20 -0800

    v7

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 6;
+const CACHE_VERSION = 7;
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
 const RUBBER_INDEX_FILE = 'stats/rubbers/index.json';
