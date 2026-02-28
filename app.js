396d720345a6d61db35a6b39ed64578e496cf7c7
aiminilabs0 <aiminilabs0@gmail.com>
2026-02-27 17:47:25 -0800

    change player format from jpg to png

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 5;
+const CACHE_VERSION = 6;
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
 const RUBBER_INDEX_FILE = 'stats/rubbers/index.json';
@@ -169,7 +169,7 @@ function parsePlayerEntry(value) {
 }
 
 function playerEmojiPath(name) {
-    return 'images/players/' + name.replace(/\s+/g, ' ') + '.jpg';
+    return 'images/players/' + name.replace(/\s+/g, ' ') + '.png';
 }
 
 function renderPlayerEntryHtml(value) {
