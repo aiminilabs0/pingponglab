4ebeed01d2507edaa5cff9b0f809aa9d15afd823
aiminilabs0 <aiminilabs0@gmail.com>
2026-03-10 23:33:20 -0700

    ver

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 37;
+const CACHE_VERSION = 38;
 const LAST_MODIFIED = '2026-03-11';
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
