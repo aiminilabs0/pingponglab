64d394001ee41960949066932b17a42a6b88f248
aiminilabs0 <aiminilabs0@gmail.com>
2026-03-11 13:32:12 -0700

    ver

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 40;
+const CACHE_VERSION = 41;
 const LAST_MODIFIED = '2026-03-11';
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
