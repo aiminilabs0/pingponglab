517c21ac5275386bc1480cf3faf9850f7a200492
aiminilabs0 <aiminilabs0@gmail.com>
2026-03-11 22:39:28 -0700

    ver

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 42;
+const CACHE_VERSION = 43;
 const LAST_MODIFIED = '2026-03-12';
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
