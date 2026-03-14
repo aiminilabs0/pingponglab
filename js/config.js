f37c4be7262a53b8b17d02cadbe95b0674ad5e15
aiminilabs0 <aiminilabs0@gmail.com>
2026-03-13 20:17:39 -0700

    add player

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 61;
+const CACHE_VERSION = 62;
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
 const RUBBER_INDEX_FILE = '/stats/rubbers/index.json';
