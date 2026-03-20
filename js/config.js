c8d81bb433a83112889e6330a2a41c9413130e37
aiminilabs0 <aiminilabs0@gmail.com>
2026-03-20 16:34:44 -0700

    ver

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 73;
+const CACHE_VERSION = 74;
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
 const RUBBER_INDEX_FILE = '/stats/rubbers/index.json';
