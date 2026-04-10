65ff7ae239d108d2f033239b2b3d8e2a0d4ce48c
aiminilabs0 <aiminilabs0@gmail.com>
2026-04-09 23:24:44 -0700

    v99

--- js/config.js
+++ js/config.js
@@ -10,7 +10,7 @@ function debounce(fn, ms) {
     };
 }
 
-const CACHE_VERSION = 98;
+const CACHE_VERSION = 99;
 function v(url) { return url + (url.includes('?') ? '&' : '?') + 'v=' + CACHE_VERSION; }
 
 const RUBBER_INDEX_FILE = '/stats/rubbers/index.json';
