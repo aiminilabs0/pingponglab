f784244e87a39b5dc53fd8fe4710fd116deddbf5
aiminilabs0 <aiminilabs0@gmail.com>
2026-04-13 20:59:00 -0700

    add Glazyer 09C

--- scripts/update-description/update.sh
+++ scripts/update-description/update.sh
@@ -27,7 +27,7 @@ if [[ -z "${param//[[:space:]]/}" ]]; then
 fi
 
 # TODO:: Modify!!!!!!  (Brand)
-brand="Xiom"
+brand="Butterfly"
 
 # Read each language source file and write to the matching directory
 mkdir -p \
